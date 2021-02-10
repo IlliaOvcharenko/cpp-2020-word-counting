b928653f4943825f2784921fea9b07ea3dcc8d53
indrekis <indrekis@gmail,com>
2021-02-10 04:48:16 +0200

    PVS-Studio free usage comments added.

--- src/archive_wrapper.cpp
+++ src/archive_wrapper.cpp
@@ -1,3 +1,5 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 #include "archive_wrapper.h"
 
 ArchiveReader::ArchiveReader(const std::string& filename, size_t max_size)
